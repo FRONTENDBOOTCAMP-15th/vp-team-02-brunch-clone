5f6a718bab38ab734fef47343c598dcaa1c6b6d4
matthewb2 <bestsciencekwabe@gmail.com>
2025-11-10 14:31:54 +0900

    inital commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // 메인 페이지
+        index: 'index.html',
+        
+        // 사용자 관련
+        
+        // 게시판 관련
+        
+        // 에러 페이지
+      },
+    },
+  },
+  appType: 'mpa', // fallback 사용안함
+});
